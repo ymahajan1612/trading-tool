c444250656aced3373389bb585e648e986edc144
Yash Mahajan <86207290+ymahajan1612@users.noreply.github.com>
2024-08-17 09:40:15 +0100

    Update Data.py

--- Data.py
+++ Data.py
@@ -12,7 +12,8 @@ class StockData:
             self.data = pd.read_csv('stock_dataframe_test.csv',index_col=0)
             self.data.index = pd.to_datetime(self.data.index)
         else:
-            self.data = self.fetchData()  # Removed the unnecessary parameters
+            self.data = self.fetchData()  
+            
 
     def fetchData(self):
         url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={self.ticker}&outputsize=full&apikey={config.API_KEY}"
@@ -24,7 +25,7 @@ class StockData:
         stock_dataframe.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
         stock_dataframe = stock_dataframe.astype(float)  # convert all data to float
         stock_dataframe.index = pd.to_datetime(stock_dataframe.index)  # Convert index to datetime
-        stock_dataframe = stock_dataframe.sort_index()  # Sort the DataFrame by date
+        stock_dataframe = stock_dataframe.sort_index()  
         stock_dataframe.index.name = "Date"
 
         stock_dataframe.to_csv("stock_dataframe_test.csv")
